4b15cb8164c27bedbdf639872ac66aec3cb54cd0
anshita <Amatkan@academic.rrc.ca>
2025-06-24 23:13:22 +0530

    tried to add some images for overall testing and working

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $db = 'final_project'; // Here, we set the database name as "serverside" according to the CRUD notes.
 $username = 'root';  // Here, we set username as "serveruser" according to the CRUD notes.
-$password = 'ups98ups';  // Here, we set password as "gorgonzola7!" according to the CRUD nots.
+$password = '';  // Here, we set password as "gorgonzola7!" according to the CRUD nots.
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $username, $password);
